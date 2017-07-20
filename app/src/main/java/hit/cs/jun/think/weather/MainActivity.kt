f0d4a4a9221a3fd317a16e648b6ab85ae9a4d359
旭 <1996scarlet@gmail.com>
2017-07-21 03:11:32 +0800

    可切换城市和分享信息

--- app/src/main/java/hit/cs/jun/think/weather/MainActivity.kt
+++ app/src/main/java/hit/cs/jun/think/weather/MainActivity.kt
@@ -1,17 +1,25 @@
 package hit.cs.jun.think.weather
 
+import android.content.Intent
 import android.support.v7.widget.GridLayoutManager
 import android.widget.TextView
+import android.widget.Toast
+import com.lljjcoder.citylist.CityListSelectActivity
+import com.lljjcoder.citylist.bean.CityInfoBean
 import hit.cs.jun.think.weather.Adapter.BaseAdapter
 import hit.cs.jun.think.weather.Bean.City
 import hit.cs.jun.think.weather.Bean.Weather
 import hit.cs.jun.think.weather.Bean.cloud
 import hit.cs.jun.think.weather.Bean.now
+import hit.cs.jun.think.weather.HTTPAround.MyTemplateObserver
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity(override val layoutId: Int = R.layout.activity_main) : BaseActivity() {
 
     private lateinit var weatherAdapter: BaseAdapter<Weather>
+    private lateinit var now: City
 
     override fun init() {
 
@@ -25,8 +33,30 @@ class MainActivity(override val layoutId: Int = R.layout.activity_main) : BaseAc
             adapter = weatherAdapter
         }
 
+        location_name.setOnClickListener {
+            val intent = Intent(this, CityListSelectActivity::class.java)
+            startActivityForResult(intent, CityListSelectActivity.CITY_SELECT_RESULT_FRAG);
+        }
 
-        val city: City = (intent.getSerializableExtra("weatherData") as cloud).results[0]
+        weather_share.setOnClickListener {
+            shareToAnyWhere()
+        }
+
+        now = (intent.getSerializableExtra("weatherData") as cloud).results[0]
+
+        InitUI(now)
+    }
+
+    private fun shareToAnyWhere() {
+        val intent = Intent(Intent.ACTION_SEND)
+        intent.type = "text/plain"
+        intent.putExtra(Intent.EXTRA_SUBJECT, "分享")
+        intent.putExtra(Intent.EXTRA_TEXT, "${now.location.name} 温度:${now.now.temperature} 湿度:${now.now.humidity} 气压:${now.now.pressure} 风力等级:${now.now.wind_scale}")
+        intent.putExtra(Intent.EXTRA_TITLE, "天气信息")
+        startActivity(Intent.createChooser(intent, "请选择"))
+    }
+
+    private fun InitUI(city: City) {
         location_name.text = city.location.name
         city_last_update.text = "最后更新\n${city.last_update}"
 
@@ -49,5 +79,33 @@ class MainActivity(override val layoutId: Int = R.layout.activity_main) : BaseAc
         text_left.text = "${now.humidity}%"
         text_middle.text = "较舒适"
         text_right.text = "68/良"
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == CityListSelectActivity.CITY_SELECT_RESULT_FRAG) {
+            if (resultCode == RESULT_OK) {
+                if (data == null) {
+                    return;
+                }
+
+                val cityInfoBean = data.extras.getParcelable<CityInfoBean>("cityinfo") ?: return
+
+                cloudAPI.getCities(location = cityInfoBean.name)
+                        .subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribe(object : MyTemplateObserver<cloud>() {
+
+                            override fun onError(e: Throwable) {
+                                Toast.makeText(baseContext, "API暂不支持此地区", Toast.LENGTH_SHORT).show()
+                            }
+
+                            override fun onNext(t: cloud) {
+                                now = t.results[0]
+                                InitUI(now)
+                            }
+                        })
+            }
+        }
     }
 }
